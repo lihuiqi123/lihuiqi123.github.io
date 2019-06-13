fac058cab73a5f542d51a93e946b9b32bd7afa4a
lihuiqi123 <50825520+lihuiqi123@users.noreply.github.com>
2019-06-13 21:06:24 +0800

    Update light.ino

--- light.ino
+++ light.ino
@@ -3,9 +3,9 @@
 int redPin= 7;
 int greenPin = 6;
 int bluePin = 5;
-int Intensity =0;
+int Intensity =0;//定义Intensity变量
 
-#define AD5 A5
+#define AD5 A5//定义模拟口A5
 
 void setup() {
   
@@ -27,7 +27,7 @@ pinMode(redPin, OUTPUT);
 
 void loop() {
   
-// put your main code here, to run repeatedly:
+//光敏小夜灯，光线越暗，值越大；反之则越小。
   
   Intensity = analogRead(AD5);  //读取模拟口AD5的值，存入Intensity变量
   Serial.print("Intensity = ");  //串口输出"Intensity = "
@@ -36,10 +36,12 @@ void loop() {
 if(Intensity>600){
   setColor(0, 0, 255); // Blue Color
   delay(1000);
+  //大于600时，即达到了一定暗的程度，亮灯
   }
   else {
     setColor(0, 0, 0); // Black Color
   delay(1000);
+    //否则，灭灯
     }
 
 }
